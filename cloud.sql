6158ae3d20fd3dd43cec8326ec45008eacbaf051
Codertransform <18204099093@163.com>
2020-01-14 17:39:39 +0800

    更新数据库备份文件

---
+++ cloud.sql
@@ -0,0 +1,37 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 50718
+Source Host           : localhost:3306
+Source Database       : cloud
+
+Target Server Type    : MYSQL
+Target Server Version : 50718
+File Encoding         : 65001
+
+Date: 2020-01-14 17:38:35
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for cars
+-- ----------------------------
+DROP TABLE IF EXISTS `cars`;
+CREATE TABLE `cars` (
+  `id` varchar(20) NOT NULL,
+  `cardId` varchar(10) NOT NULL COMMENT '车牌号',
+  `brandName` varchar(20) NOT NULL COMMENT '品牌名',
+  `model` char(1) NOT NULL COMMENT '车型',
+  `color` char(1) NOT NULL COMMENT '颜色',
+  `carType` char(1) NOT NULL COMMENT '车辆类型',
+  `status` char(1) NOT NULL COMMENT '状态',
+  `buy_time` datetime NOT NULL COMMENT '购买时间',
+  `maintenance` datetime DEFAULT NULL COMMENT '上次保养时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='车辆信息表';
+
+-- ----------------------------
+-- Records of cars
+-- ----------------------------
