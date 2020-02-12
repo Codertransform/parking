c0d146f6e3c23d083fc4860bb29f35e2ef1c351d
wzy <18204099093@163.com>
2020-02-13 01:28:17 +0800

    数据库备份

--- cloud.sql
+++ cloud.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50718
 File Encoding         : 65001
 
-Date: 2020-02-09 00:08:09
+Date: 2020-02-13 01:27:59
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -24,8 +24,8 @@ CREATE TABLE `cars` (
   `cardId` varchar(10) NOT NULL COMMENT '车牌号',
   `model` varchar(20) NOT NULL COMMENT '型号',
   `color` varchar(20) NOT NULL COMMENT '颜色',
-  `carType` char(1) NOT NULL COMMENT '车辆类型',
-  `status` char(1) NOT NULL COMMENT '状态',
+  `carType` char(2) NOT NULL COMMENT '车辆类型',
+  `status` char(2) NOT NULL COMMENT '状态',
   `buy_time` datetime NOT NULL COMMENT '购买时间',
   `maintenance` datetime DEFAULT NULL COMMENT '上次保养时间',
   PRIMARY KEY (`id`)
@@ -34,6 +34,18 @@ CREATE TABLE `cars` (
 -- ----------------------------
 -- Records of cars
 -- ----------------------------
-INSERT INTO `cars` VALUES ('c880639e9cf0422c8eee49d69bc314d6', '陕C·V5591', '静安寺独立开发', '白色', '0', '0', '2020-02-07 15:22:41', '2020-02-07 15:22:43');
-INSERT INTO `cars` VALUES ('e817fa1e3cee4f8496c7c2b937aaa7eb', '房东是个很', '看见啊大号封了你', '啊的风格啊', '0', '0', '2020-01-17 09:31:02', '2020-01-17 09:29:27');
-INSERT INTO `cars` VALUES ('ff02a2cb0a6e48d4adb2974c63bd75ad', '陕C·7797W', '斯巴鲁123', '灰色', '0', '0', '2020-02-07 15:22:41', '2020-02-07 15:22:43');
+INSERT INTO `cars` VALUES ('5cbd1e936bfd48f783e28859cde904d6', '陕C·7797W', '斯巴鲁123', '灰色', '0', '0', '2020-02-07 21:14:11', '2020-02-07 15:22:43');
+INSERT INTO `cars` VALUES ('d8774db7b1914a9daefb3ff85301ccc7', '陕C·V5591', '静安寺独立开发', '白色', '0', '-1', '2020-02-07 15:22:41', '2020-02-07 15:22:43');
+
+-- ----------------------------
+-- Table structure for dict
+-- ----------------------------
+DROP TABLE IF EXISTS `dict`;
+CREATE TABLE `dict` (
+  `id` varchar(64) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of dict
+-- ----------------------------
