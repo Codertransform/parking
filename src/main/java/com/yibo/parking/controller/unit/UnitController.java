0f343c86ca2246d0994a220487eccb3861e4b56b
wzy <18204099093@163.com>
2020-03-24 00:16:25 +0800

    更新申请单位模块代码

--- src/main/java/com/yibo/parking/controller/unit/UnitController.java
+++ src/main/java/com/yibo/parking/controller/unit/UnitController.java
@@ -5,10 +5,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping(value = "/unit")
 public class UnitController {
 
     @RequestMapping(value = {"","/"})
     public String index(Model model){
-        return "";
+        return "unit/index";
     }
 }
