c1245dc8647f7398a220d081f1068f63f2a8568d
Codertransform <18204099093@163.com>
2020-05-24 23:29:13 +0800

    更新TCP解析代码

--- src/main/java/com/yibo/parking/handler/TcpDecoderHandler.java
+++ src/main/java/com/yibo/parking/handler/TcpDecoderHandler.java
@@ -11,19 +11,18 @@ import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @Service
-public class TcpDecoderHandler extends MessageToMessageDecoder<Object> {
+public class TcpDecoderHandler extends MessageToMessageDecoder<ByteBuf> {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(TcpDecoderHandler.class);
+    private static final Logger logger = LoggerFactory.getLogger(TcpDecoderHandler.class);
 
     @Override
-    protected void decode(ChannelHandlerContext channelHandlerContext, Object o, List<Object> list) throws Exception {
-        LOGGER.info("解析client上报数据");
-        System.out.println(o);
-        ByteBuf byteBuf = (ByteBuf)o;
+    protected void decode(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf, List<Object> list) throws Exception {
+        logger.info("解析client上报数据");
         byte[] data = new byte[byteBuf.readableBytes()];
         byteBuf.readBytes(data);
         String msg = new String(data, StandardCharsets.UTF_8);
-        LOGGER.info("{TCP logger print in data}: " + msg);
-        list.add(msg);
+        logger.info("{TCP logger print in data}: " + msg);
+        System.out.println(msg);
+//        list.add(msg);
     }
 }
