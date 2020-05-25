356ead59550c13b3f8d29fd2c2fb96b990922ef6
Codertransform <18204099093@163.com>
2020-05-25 17:37:28 +0800

    更新数据解析代码及工具代码

--- src/main/java/com/yibo/parking/handler/TcpDecoderHandler.java
+++ src/main/java/com/yibo/parking/handler/TcpDecoderHandler.java
@@ -1,6 +1,8 @@
 package com.yibo.parking.handler;
 
+import com.yibo.parking.utils.HexConvert;
 import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToMessageDecoder;
 import org.slf4j.Logger;
@@ -11,6 +13,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @Service
+@ChannelHandler.Sharable
 public class TcpDecoderHandler extends MessageToMessageDecoder<ByteBuf> {
 
     private static final Logger logger = LoggerFactory.getLogger(TcpDecoderHandler.class);
@@ -21,8 +24,31 @@ public class TcpDecoderHandler extends MessageToMessageDecoder<ByteBuf> {
         byte[] data = new byte[byteBuf.readableBytes()];
         byteBuf.readBytes(data);
         String msg = new String(data, StandardCharsets.UTF_8);
-        logger.info("{TCP logger print in data}: " + msg);
-        System.out.println(msg);
+        String[] split = msg.split(",");
+        String first = split[0];
+        if (first.contains("*")) {
+            switch (split[2]){
+                case "V1":
+                    System.out.println(split[5]);
+                    System.out.println(split[7]);
+                    break;
+                case "V2" :
+                    break;
+                default:
+                    System.out.println(split[5]);
+                    System.out.println(split[7]);
+                    break;
+            }
+        }
+        if (first.contains("$")){
+            String hexString = HexConvert.BinaryToHexString( data );
+            hexString =  hexString.replace( " ","" );//去除空格
+            System.out.println(hexString);
+            String head = hexString.substring(0,1);
+            System.out.println(head);
+            String asc = HexConvert.convertHexToString( head );//转为ASCII,如：*00007VERSION\n1$
+            System.out.println("收到发来的消息：" + asc);
+        }
 //        list.add(msg);
     }
 }
