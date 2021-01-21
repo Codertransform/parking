52fd7c7ef3ac2e3b3a1e653b92539405cc633373
Codetransform <18204099093@163.com>
2021-01-21 17:34:03 +0800

    更新轨迹点数据获取代码

--- src/main/java/com/yibo/parking/service/Impl/car/TrackServiceImpl.java
+++ src/main/java/com/yibo/parking/service/Impl/car/TrackServiceImpl.java
@@ -42,12 +42,27 @@ public class TrackServiceImpl implements TrackService {
         map.put("trid",track.getTrackId());
         map.put("pagesize","999");
         map.put("correction", "denoise=1,mapmatch=1");
-        String result = HttpClientUtil.doPost(url,map);
+        String result = HttpClientUtil.doGet(url,map);
         JSONObject object = JSONObject.parseObject(result);
+        System.out.println(object);
         JSONArray array = new JSONArray();
         if (object.get("errcode").toString().equals("10000")) {
             JSONObject jsonObject = object.getJSONObject("data");
             array = jsonObject.getJSONArray("tracks");
+            JSONObject json = array.getJSONObject(0);
+            int count = Integer.parseInt(json.get("counts").toString());
+            if (count > 1000){
+                Map<String,String> m = new HashMap<>();
+                map.put("key","1c92d37732848ca864c4daac21454294");
+                map.put("sid",dev.getsId());
+                map.put("tid",dev.gettId());
+                map.put("trid",track.getTrackId());
+                map.put("page","2");
+                map.put("correction", "denoise=1,mapmatch=1");
+                String res = HttpClientUtil.doGet(url,m);
+                JSONObject o = JSONObject.parseObject(res);
+                System.out.println(o);
+            }
         }
         return array;
     }
