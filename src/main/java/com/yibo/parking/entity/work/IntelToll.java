51d3dc11c357bc70dde5190887a6135edf1b26cc
Codetransform <18204099093@163.com>
2021-04-30 17:26:16 +0800

    更新智能化收费员部分功能

--- src/main/java/com/yibo/parking/entity/work/IntelToll.java
+++ src/main/java/com/yibo/parking/entity/work/IntelToll.java
@@ -3,12 +3,14 @@ package com.yibo.parking.entity.work;
 public class IntelToll {
     private String id;
     private String code;        //工号
-    private String posId;        //工号
+    private String posId;        //终端号
     private String name;        //姓名
+    private String idNum;        //身份证号
     private String birthday;    //出生日期
     private String tel;         //电话
     private String address;     //住址
     private String supplies;    //配发物资
+    private String joinTime;    //入职时间
     private String remarks;     //备注
 
     public String getId() {
@@ -43,6 +45,14 @@ public class IntelToll {
         this.name = name;
     }
 
+    public String getIdNum() {
+        return idNum;
+    }
+
+    public void setIdNum(String idNum) {
+        this.idNum = idNum;
+    }
+
     public String getBirthday() {
         return birthday;
     }
@@ -73,6 +83,14 @@ public class IntelToll {
 
     public void setSupplies(String supplies) {
         this.supplies = supplies;
+    }
+
+    public String getJoinTime() {
+        return joinTime;
+    }
+
+    public void setJoinTime(String joinTime) {
+        this.joinTime = joinTime;
     }
 
     public String getRemarks() {
